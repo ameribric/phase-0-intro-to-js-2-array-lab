dee9ffbbcb3b92ccdc72b0dde51d5c7e457e5876
Amer Ibric <ameribric@gmail.com>
2022-03-01 15:43:15 -0500

    commit tree 923b0057a7510e7cb2406c05e88b5cd72f9e8380

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat (name) {
+    return cats.unshift(name) 
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats] 
+}
+
+
+function removeLastCat(name) {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+}
+
+
+// cats.unshift()
+
+// cats.pop()
+
+// cats.unshift()
+
+
+
+// const copyOfCats = cats.slice()
+
+// const copyOfCats = cats.slice(-1)
+
+// const copyOfCats = cats.slice(1,2)
+
+// const copyOfCats = cats.slice (0)
+
+// const copyOfCatsTwo = [...cats];
+
+// const firstCats = [...cats, "Milo"]
+
+// const lastCats = ("Garfield", ...cats)
